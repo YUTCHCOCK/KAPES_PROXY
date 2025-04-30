12a39dd28708314d674cf72b43bc5c3a41230ddc
YUTCHCOCK <yutchcock@gmail.com>
2025-04-30 23:51:52 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
-const NOTION_SITE = "daffy-tarsier-75a.notion.site";
-const NOTION_PAGE_ID = "1de723f7d2c180b7979ef9b150714085";
+const NOTION_URL = "https://daffy-tarsier-75a.notion.site/1de723f7d2c180b7979ef9b150714085?pvs=4";
 
 export default {
   async fetch(request) {
-    const url = new URL(request.url);
-    const notionUrl = `https://${NOTION_SITE}/${NOTION_PAGE_ID}${url.pathname}${url.search}`;
-
-    const response = await fetch(notionUrl, {
-      method: request.method,
-      headers: {
-        "user-agent": request.headers.get("user-agent") || "",
-        "accept": request.headers.get("accept") || "*/*",
-        "accept-language": request.headers.get("accept-language") || "en-US,en;q=0.9",
-      },
-    });
-
-    return new Response(response.body, {
-      status: response.status,
-      headers: {
-        "content-type": response.headers.get("content-type") || "text/html",
-      },
-    });
-  }
+    return Response.redirect(NOTION_URL, 302);
+  },
 };

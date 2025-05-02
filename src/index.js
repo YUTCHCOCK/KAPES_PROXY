7267b10bebc962280e859778736481b535f54b1f
YUTCHCOCK <yutchcock@gmail.com>
2025-05-02 12:25:33 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-const NOTION_URL = "https://daffy-tarsier-75a.notion.site/1de723f7d2c180b7979ef9b150714085?pvs=4";
+const NOTION_URL = "https://daffy-tarsier-75a.notion.site/1de723f7d2c180b7979ef9b150714085";
 
 export default {
   async fetch(request) {
-    return Response.redirect(NOTION_URL, 302);
+    return Response.redirect(NOTION_URL, 301);
   },
 };
